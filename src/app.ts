9d3b750e7595e6410437077c75866728c14ebca2
Connor Bunch <connorbunch@outlook.com>
2024-03-23 18:54:56 -0600

    added placeholder express app written in typescript

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,24 @@
-import express from 'express';
+import express, {Request, Response, NextFunction} from 'express';
+import HttpError from './utils/HttpError';
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.use((req, res, next) => {
+    const error = new HttpError('The requested resource does not exist.', 404);
+    next(error);
+});
+
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    res.status(err.status);
+    res.json({
+        'Error': err.message,
+        'Status': err.status
+    });
+    console.error(err);
 });
 
 export default app;
